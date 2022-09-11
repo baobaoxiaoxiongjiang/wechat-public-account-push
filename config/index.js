e998e54b23b7fdbbe5f11237e798dfaedb348fa0
baobaoxiaoxiongjiang <113273343+baobaoxiaoxiongjiang@users.noreply.github.com>
2022-09-11 12:56:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx8d64e6a5bb8c8e8d",
+  APP_ID: "",
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github scret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "f391db396e91ebd012d0d334b2e760ad",
+  APP_SECRET: "",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -20,9 +20,9 @@ export const config = {
    *    // 想要发送的人的名字
    *    name: "老婆0",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "o9ZKR6lvhFFnAWA998vv5-zK11VI",
+   *    id: "",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "Qg9qDWhtWlzPqS3QdUj639yEmrF3wBE5tiULBXA5LE0",
+   *    useTemplateId: "",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   },
@@ -33,14 +33,14 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o9ZKR6lvhFFnAWA998vv5-zK11VI",
+      id: "",
 ",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Qg9qDWhtWlzPqS3QdUj639yEmrF3wBE5tiULBXA5LE0",
+      useTemplateId: "",
       // 所在省份
-      province: "河南",
+      province: "北京",
       // 所在城市
-      city: "河南",
+      city: "北京",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
